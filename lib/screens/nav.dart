ff197b775090172bc32972f63d0321a26a0fd086
Farhan Athaullah <athaullahfarhan@gmail.com>
2021-10-30 22:14:21 +0700

    Adding bottom navigation bar in nav.dart

---
+++ lib/screens/nav.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mod3_kel02/screens/home2.dart';
+import 'package:mod3_kel02/screens/profile.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({Key? key}) : super(key: key);
+
+  @override
+  _NavigationState createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  final screens = [
+    Home(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Weeb App'),
+          elevation: 0,
+          backgroundColor: Colors.transparent),
+      body: screens[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: false,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
