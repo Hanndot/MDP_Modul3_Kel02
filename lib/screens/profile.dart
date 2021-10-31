c671878cdad531e035813cf90b23d8584431aea5
Farhan Athaullah <athaullahfarhan@gmail.com>
2021-10-31 12:41:49 +0700

    Finishing profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  final List nama = [
+    "Anadda Ferrell Ramadhan",
+    "Nisrina Shofa Nadifa",
+    "Indriawan Muhammad Akbar",
+    "M. Farhan Athaullah",
+  ];
+  final List nim = [
+    "(21120119130035)",
+    "(21120119120002)",
+    "(21120119130070)",
+    "(21120119130072)",
+  ];
 
+  ProfilePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 30, bottom: 15),
-            child: Text(
-              'Kelompok 02',
-              style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold, fontSize: 45),
-            ),
-          ),
-        ),
-        Text('M. Farhan Athaullah (21120119130072)')
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+                title: Text(nama[index],
+                    style: const TextStyle(color: Colors.white)),
+                subtitle: Text(nim[index],
+                    style: const TextStyle(color: Colors.white)),
+                leading: CircleAvatar(
+                  child: Text(nama[index][0],
+                      style: const TextStyle(color: Colors.white)),
+                )),
+          );
+        },
+        itemCount: nama.length,
+      ),
     );
   }
 }
